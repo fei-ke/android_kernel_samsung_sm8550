c0069cfa8bb12e643e4f626f605cea1aec843d10
fei-ke <jinyang656@gmail.com>
2024-02-10 15:01:25 +0000

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#
+# Tested on Ubuntu 20.04
+#
+# 1. Install Clang/LLVM
+#
+# apt install -y clang llvm lld
+#
+# Or get Clang/LLVM from https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r450784e.tar.gz 
+# and add bin dir to PATH
+#
+# 2. Install required packages
+# apt install -y git zip make xz-utils bison flex libz-dev libssl-dev libelf-dev bc cpio python3 pahole
+#
+# 3. Run build.sh to build kernel, do a clean before build is recommended
+# make clean && rm -rf ./out
+# ./build.sh <your defconfig>
+# 
+# 4. Image output to out/arch/arm64/boot/Image
+# 
+
+set -e
+
+TARGET_DEFCONFIG=${1:-kalama_gki_defconfig}
+
+cd "$(dirname "$0")"
+
+ARGS='
+CC=clang
+ARCH=arm64
+LLVM=1 LLVM_IAS=1
+LOCALVERSION=-android13-8
+'
+make -j$(nproc) -C $(pwd) O=$(pwd)/out ${ARGS} $TARGET_DEFCONFIG
+make -j$(nproc) -C $(pwd) O=$(pwd)/out ${ARGS}
